e96b28532f4bf3cb4cdcad4da68976c1eafb55d1
lauraPmansilla <146298020+lauraPmansilla@users.noreply.github.com>
2023-11-08 11:36:55 -0300

    Update app.js para poder hacer deployment

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const PORT = process.env.PORT ?? 3000
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando en el puerto 3000');
+app.listen(PORT, () => {
+    console.log("Servidor funcionando en http://localhost: " + PORT);
 });
 
 app.get('/', (req,res)=>{
